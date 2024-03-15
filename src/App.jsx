d7242326c854ea2b2dd8ef0d25e2d765640ff15a
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-03-16 05:00:50 +0600

    Some Correction in css

--- src/App.jsx
+++ src/App.jsx
@@ -46,12 +46,12 @@ const [totalCalories, setTotalCalories] = useState(0)
         <h1 className='pt-24 text-center text-[#150B2B] text-[40px] font-semibold'>Our Recipes</h1>
         <p className='pb-12 px-6 lg:px-[185px] text-center text-[#150b2b99]'>Explore our curated collection of diverse dishes, from appetizers to desserts, and unlock the secrets to creating delicious meals that will impress every palate.</p>
       </div>
-      <div className='grid grid-cols-1 lg:grid-cols-5 gap-6'>
+      <div className='grid grid-cols-1 lg:grid-cols-5 gap-0 lg:gap-6'>
         <div className='col-span-3'>
           <RecipeCards handleWantToCook={handleWantToCook}></RecipeCards>
           <ToastContainer/>
         </div>
-        <div className='col-span-2'>
+        <div className='col-span-2 mt-4 lg:mt-0'>
           <RecipeTables recipes={recipes} handleCurrentCooking={handleCurrentCooking} currentRecipes={currentRecipes} totalTime={totalTime} totalCalories={totalCalories}></RecipeTables>
         </div>
       </div>
